cb7ea93a1b75fa039b3d5ba8e5f15312e80dc9ae
unknown <younews372@gmail.com>
2023-06-21 23:38:35 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+
+const about = () => {
+  return ( 
+    <div>
+      <h1>About</h1>
+      <p>Loerem insum sit amet</p>
+      <p>Lorem insum sit amet</p>
+    </div>
+   );
+}
+ 
+export default about;
---
+++ test-app5/src/pages/about.js
@@ -0,0 +1,3 @@
+export default function About() {
+    return <h1>test app 5</h1>
+  }
