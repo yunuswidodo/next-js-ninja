0f16466090ffdfbbc0f5887e85b48bd377fd1f67
unknown <younews372@gmail.com>
2023-07-15 00:44:02 +0700

    test index

--- pages/yunus/index.js
+++ pages/yunus/index.js
@@ -5,18 +5,19 @@ export const getStaticProps  = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
 
+    // console.log(data);
     return {
-        props: {yunus : data}
+        props: {nana : data}
     }
 }
 
 
-const Yunus = ({yunus}) => {
+const Yunus = ({nana}) => {
 
     return ( 
         <div>
             <h1>Hallo Ini adalah List Yunsu</h1>
-            {yunus.map(yunus => (
+            {nana.map(yunus => (
                 
                     <Link href={'/yunus/' + yunus.id} key={yunus.id} className={Style.single}>
                         {/* <a className={Style.single}> */}
